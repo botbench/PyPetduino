14bc9bdd9626cb94491b9f839f910a3d00b7c0e9
Xander Soldaat <xander@botbench.com>
2015-08-24 08:34:07 +0200

    Added simply Temperature example Removed non-LED related stuff from BlinkLed.py

--- Examples/GetTemperature.py
+++ Examples/GetTemperature.py
@@ -5,17 +5,16 @@ import sys
 
 def main():
 	try:
-		pet = Petduino.Petduino('COM5', 9600)
+		pet = Petduino.Petduino('COM4', 9600)
 	except Petduino.UnhappyPetduino as sadness:
 		print "There is a disturbance in the force: %s" % sadness
 		sys.stdin.read()
 		sys.exit()
 
 	while True:
-		pet.setLed(1)
+		pet.getTemperature()
 		sleep(1)
-		pet.setLed(0)
-		sleep(1)
+
 
 if __name__ == "__main__":
   main()
